12cfde98df018b556811670ebbc1b4130d01bfea
Matthew Dyer <matthew.dyer@aura.org.nz>
2014-07-25 13:16:18 +1200

    inital commit

---
+++ src/main/java/helloworldspring/Greeting.java
@@ -0,0 +1,24 @@
+package helloworldspring;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    
+    public String getStaticContent(){
+    	return " {content:\"I am also JSON!\"}";
+    }
+}
